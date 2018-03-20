3dbb56393d75b7922aab0856888fede3cd161fd0
Jared Kibele <jkibele@gmail.com>
2018-03-20 12:42:53 -0700

    My name in the copyright

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pyriv'
-copyright = u"2017, Haleigh Wright"
+copyright = u"2017, Jared Kibele"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout

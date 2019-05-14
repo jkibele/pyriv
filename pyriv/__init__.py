15e4bab62eaf7de5d58710906e98f5025ed6e594
Jared Kibele <jkibele@gmail.com>
2019-05-14 16:27:35 -0700

    Trying to fix imports

--- pyriv/__init__.py
+++ pyriv/__init__.py
@@ -1,10 +1,10 @@
-import common
+from . import common
 from river_graph import RiverGraph
 from mad_graph import MadGraph
 from graph_prep import GraphBuilder
 from river_dist import RiverDist
 from land import Land, CoastLine
-import units
+from . import units
 
 __author__ = """Jared Kibele"""
 __email__ = 'jkibele@nceas.ucsb.edu'
